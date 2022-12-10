7e67d3f25a966f68a1a7578cca1cf1812e0deb9c
Alex Hsieh <alexchsieh@yahoo.com>
2022-12-10 08:02:23 -0800

    modifying pseudo for main search

--- main.py
+++ main.py
@@ -4,51 +4,51 @@ import pandas as table
 def feature_search(data, alg):
 
     current_set_of_features = []
-    seen = set()
 
     for i in range(1, len(data[0])):
-        print('On the ', num2str(i), ' th level of the search tree')
-        feature_to_add_at_this_level = []
+        print('On the ', i, ' th level of the search tree')
+        feature_to_add_at_this_level = 0
         best_so_far_accuracy = 0
 
         for k in range(1, len(data[0])):
-            if k not in seen:
-                print(' -- Considering adding the ', num2str(k), ' feature')
+            if k not in current_set_of_features:
+                print(' -- Considering adding the ', k, ' feature')
                 accuracy = leave_one_out_cross_validation(
-                    data, current_set_of_features, k+1)
+                    data, current_set_of_features, k, alg)
 
-                if accuracy > best_so_far_accuracy
-                best_so_far_accuracy = accuracy
+                print(f'---Using features {current_set_of_features} and adding {k} has an accuracy of {round(accuracy * 100, 1)}%') if alg == 1 else print(
+                    f'---Removing features {current_set_of_features} and also {k} has an accuracy of {round(accuracy * 100, 1)}%')
+
+                if accuracy > best_so_far_accuracy:
+                    best_so_far_accuracy = accuracy
+                    feature_to_add_at_this_level = k
 
         current_set_of_features.append(feature_to_add_at_this_level)
-        print('On level ', num2str(i), ' i added feature ', num2str(
-            feature_to_add_at_this_level), ' to current set')
+        print('On level ', i, ' i added feature ',
+              feature_to_add_at_this_level, ' to current set')
 
 
 def leave_one_out_cross_validation(data, current_set_of_features, add_feature, alg):
     number_correctly_classified = 0
 
-    for i = 1:
-        size(data, 1)
+    for i in range(1, len(data[0])):
         object_to_classify = data(i, 2: end)
         label_object_to_classify = data(i, 1)
-
         nearest_neighbor_distance = float('inf')
         nearest_neighbor_location = float('inf')
 
-        for k = 1:
-            size(data, 1)
-            if k ~ = i
-            distance = sqrt(sum((object_to_classify - data(k, 2: end)). ^ 2))
-            if distance < nearest_neighbor_distance
-            nearest_neighbor_distance = distance
-            nearest_neighbor_location = k
-            nearest_neighbor_label = data(nearest_neighbor_location, 1)
+        for k in range(1, len(data[0])):
+            if k != i:
+                distance = sqrt(sum((object_to_classify - data(k, 2: end)). ^ 2))
+            if distance < nearest_neighbor_distance:
+                nearest_neighbor_distance = distance
+                nearest_neighbor_location = k
+                nearest_neighbor_label = data(nearest_neighbor_location, 1)
 
-        if label_object_to_classify == nearest_neighbor_label
-        number_correctly_classified = number_correctly_classified + 1
+        if label_object_to_classify == nearest_neighbor_label:
+            number_correctly_classified = number_correctly_classified + 1
 
-    accuracy = number_correctly_classified / size(data, 1)
+    accuracy = number_correctly_classified / len(data[0])
 
 
 def main():
